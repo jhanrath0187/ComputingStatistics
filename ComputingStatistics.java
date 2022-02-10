436f4000398ec6e4811827baf0a4377c605b127f
Justin <jhanrath0187@myduneland.org>
2022-02-10 08:59:01 -0600

    formatting

--- ComputingStatistics.java
+++ ComputingStatistics.java
@@ -40,4 +40,38 @@ public class ComputingStatistics {
        avg=this.totalAmount()/data.size();
        return avg;
    }
+   public double largestLoan() {
+      double largestLoan = 0.0;
+      Loan loan;
+      for(int i = 0; i < data.size(); i++) {
+         loan = data.get(i);
+         if (loan.getLoanAmount() > largestLoan)
+             largestLoan = loan.getLoanAmount();
+      }
+      return largestLoan;
+   }
+      public double smallestLoan() {
+      double smallestLoan = 100000.0;
+      Loan loan;
+      for(int i = 0; i < data.size(); i++) {
+         loan = data.get(i);
+         if (loan.getLoanAmount() < smallestLoan)
+             smallestLoan = loan.getLoanAmount();
+      }
+      return smallestLoan;
+   }
+   public double largestLoan(String country)
+   {
+      double largestLoan = 0.0;
+      Loan loan;
+      for(int i = 0; i < data.size(); i++) {
+         loan = data.get(i);
+         if ((loan.getCountry()).equals(country))
+         {
+             if (loan.getLoanAmount() > largestLoan)
+                 largestLoan = loan.getLoanAmount();
+         }
+      }
+      return largestLoan;
+   }
 }
